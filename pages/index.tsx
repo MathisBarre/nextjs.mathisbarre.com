4725e98808a08c696ceb6bc477fc6886b282d58b
Mathis Barré <mathis.barre@live.fr>
2021-01-05 14:32:11 +0100

    feat: project init

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen bg-blue-800">
+        <h1 className="text-6xl font-bold text-white">Ready to get started !</h1>
+      </div>
+    </>
+  )
+}
