2f302238a2f1283d64678a28e27cbabca92baa0e
Mathis Barré <mathis.barre@live.fr>
2021-01-05 16:03:49 +0100

    feat: hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,34 @@
+// IMPORT NEXT.JS COMPONENTS --------//
+import Image from "next/image"
+
+// IMPORT COMPONENTS ----------------//
+import Credit from "@/components/Credit"
+
 export default function Hero() {
   return (
-    <>
-      <h1>Hero !</h1>
-    </>
+    <header className="flex items-center justify-center bg-gray-900 h-192 sm:h-144 bg-hero bg-cover bg-center relative">
+      <div className="hidden sm:flex sm:items-center ">
+        <Image
+          className="hidden rounded-full"
+          src="/images/profil.jpg"
+          layout="fixed"
+          height="120"
+          width="120"
+          quality="100"
+          priority
+        />
+      </div>
+      <div className="flex flex-col px-4">
+        <div className="flex flex-col">
+          <p className="text-white text-5xl font-black">Mathis Barré</p>
+          <p className="text-white text-3xl mb-2 tracking-widest">Développeur web</p>
+          <div className="flex flex-col sm:flex-row">
+            <button className="text-white text-lg bg-primary rounded py-0.5 px-4 mb-2 sm:mb-0 sm:mr-2 sm:w-44">Voir mes projets</button>
+            <button className="text-white text-lg border border-white bg-black bg-opacity-30 rounded py-0.5 px-4 sm:w-44">Me contacter</button>
+          </div>
+        </div>
+      </div>
+      <p className="absolute bottom-0 right-0 text-white text-opacity-20 p-2"><Credit/></p>
+    </header>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '192': '48rem',
+        '144': '36rem'
+      },
+      colors: {
+        'primary': '#f50057',
+        'darkPrimary': '#cc1355'
+      },
+      backgroundImage: theme => ({
+        'hero': "url('/images/john-towner-JgOeRuGD_Y4-unsplash.jpg')",
+      })
+    },
   },
   variants: {
     extend: {},
