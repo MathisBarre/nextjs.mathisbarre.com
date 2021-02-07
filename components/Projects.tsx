9b86d79c2e60d01adf51e7a1a34d306f8ba722ba
Mathis Barré <mathis.barre@live.fr>
2021-02-07 16:12:51 +0100

    feat: start projects section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,7 +1,34 @@
 export default function Projects() {
   return (
-    <>
-      <h1>Projects !</h1>
-    </>
+    <section className="max-w-5xl mx-auto">
+      <div className="container mx-auto py-16">
+        <h2 className="text-3xl font-bold text-center mb-16">Mes derniers projet<span className="text-primary">.</span></h2>
+        <h3 className="text-xl mb-4 mt-8">Relatif à OpenClassrooms</h3>
+        <div className="grid grid-cols-3 gap-4">
+          <Project/>
+          <Project/>
+          <Project/>
+        </div>
+        <h3 className="text-xl mb-4 mt-8">En tant que freelance</h3>
+        <div className="grid grid-cols-3 gap-4">
+          <Project/>
+          <Project/>
+          <Project/>
+        </div>
+        <h3 className="text-xl mb-4 mt-8">Side projects</h3>
+        <div className="grid grid-cols-3 gap-4">
+          <Project/>
+          <Project/>
+          <Project/>
+        </div>
+      </div>
+    </section>
+  )
+}
+function Project() {
+  return (
+    <div className="aspect-w-16 aspect-h-9 bg-gray-900 rounded">
+
+    </div>
   )
 }
