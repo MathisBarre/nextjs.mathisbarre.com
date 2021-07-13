f2e80a816602783f9fd5d41552128721c1183887
Mathis Barré <mathis.barre@live.fr>
2021-07-13 12:26:45 +0200

    fix: remove old service worker

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,18 @@
 import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document'
-
+import { useEffect } from "react"
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
 
     return initialProps;
+  }
+
+  componentDidMount() {
+    navigator.serviceWorker.getRegistrations().then(function (registrations) {
+      for (let registration of registrations) {
+        registration.unregister()
+      }
+    })
   }
 
   render() {
@@ -14,6 +22,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+
         </Head>
         <body>
           <Main />
