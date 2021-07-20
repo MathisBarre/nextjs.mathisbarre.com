3afad42040e5b143839652b02c3998e4e873c465
Mathis Barré <mathis.barre@live.fr>
2021-07-20 19:31:33 +0200

    feat: preview image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
-import "tailwindcss/tailwind.css";
-import '../styles/globals.css'
 import { AppProps } from 'next/app'
+import Head from "next/head"
+import "tailwindcss/tailwind.css"
+import '../styles/globals.css'
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta property="og:image" content="https://mathisbarre.com/images/overview.png" />
+        <meta property="twitter:image" content="https://mathisbarre.com/images/overview.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
