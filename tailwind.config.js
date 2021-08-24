cf49c138d15c4167f3950c7cc09bcb299a032e5b
Mathis Barré <mathis.barre@live.fr>
2021-08-24 21:15:00 +0200

    feat: enabled jit mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,25 @@
 module.exports = {
-  purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
+  purge: ["./pages/**/*.tsx", "./components/**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
+  mode: "jit",
   theme: {
     extend: {
       spacing: {
-        '192': '48rem',
-        '144': '36rem'
+        192: "48rem",
+        144: "36rem",
       },
       colors: {
-        'primary-100': '#f50057',
-        'primary-200': '#cc1355',
-        'primary-800': '#581a28',
+        "primary-100": "#f50057",
+        "primary-200": "#cc1355",
+        "primary-800": "#581a28",
       },
-      backgroundImage: theme => ({
-        'hero': "url('/images/john-towner-JgOeRuGD_Y4-unsplash.jpg')",
-      })
+      backgroundImage: (theme) => ({
+        hero: "url('/images/john-towner-JgOeRuGD_Y4-unsplash.jpg')",
+      }),
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/aspect-ratio'),
-  ],
-}
+  plugins: [require("@tailwindcss/aspect-ratio")],
+};
