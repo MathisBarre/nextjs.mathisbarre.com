92e8b196bf7ef220786f41f9562fb1ca96527393
Mathis Barré <mathis.barre@live.fr>
2021-08-24 21:15:11 +0200

    feat: section "join me"

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head"
+import Image from "next/image"
 
 export default function Home() {
   return (
@@ -46,16 +47,42 @@ export default function Home() {
 
         <h2 className="mt-12 mb-2 text-xl font-bold sm:text-3xl">What are the technologies I'm good with ?</h2>
         <p className="mb-2 text-gray-200">
-          I do love working with <span className="font-bold text-pink-500">tailwindcss</span>, <span className="font-bold text-pink-500">React</span> and <span className="font-bold text-pink-500">Next.js</span> when I'm doing front-end web development. But I also learned <span className="font-bold text-pink-500">Vue.js</span> during my internship in Lion Coach. I do use <span className="font-bold text-pink-500">TypeScript</span> with long-term projects.
+          I do love working with <span className="font-bold text-pink-500">tailwindcss</span>, <span className="font-bold text-pink-500">React</span> and <span className="font-bold text-pink-500">Next.js</span> when I'm doing front-end web development. But I also learned <span className="font-bold text-pink-500">Vue.js</span> during my internship at Lion Coach. I do use <span className="font-bold text-pink-500">TypeScript</span> with long-term projects.
         </p>
 
         <p className="mb-2 text-gray-200">
-          On the back-end development side, I learned <span className="font-bold text-pink-500">Express.js</span> but I plan to switch to <span className="font-bold text-pink-500">Fastify</span> for low scope development. When I need a more powerfull framework, I use <span className="font-bold text-pink-500">Adonis</span>. I do know basic usage of SQL and NoSQL database.
+          On the back-end development side, I know <span className="font-bold text-pink-500">Express.js</span> but I prefer <span className="font-bold text-pink-500">Fastify</span> with <span className="font-bold text-pink-500">Prisma ORM</span> and <span className="font-bold text-pink-500">MySQL</span>. I do know basic usage of SQL and NoSQL database.
         </p>
 
         <p className="text-gray-200">
           I don't do mobile development for now but I plan to learn <span className="font-bold text-pink-500">React Native</span>, <span className="font-bold text-pink-500">Flutter</span> or <span className="font-bold text-pink-500">Ionic</span> with <span className="font-bold text-pink-500">Capacitor</span>.
         </p>
+
+        <section>
+          <h2 className="mt-12 mb-2 text-xl font-bold sm:text-3xl">Join me on</h2>
+
+          <div className="grid grid-cols-3 gap-2 sm:grid-cols-6">
+            <a href="https://github.com/MathisBarre">
+              <div className="duration-75 flex items-center justify-center h-16 border border-gray-600 rounded cursor-pointer hover:bg-gray-800 hover:bg-[#181717] hover:border-none"><Image width="24" height="24" src="/images/icons/github.svg" /></div>
+            </a>
+            <a href="https://linkedin.com/in/mathis-barre">
+              <div className="duration-75 flex items-center justify-center h-16 border text-white fill-current border-gray-600 rounded cursor-pointer hover:bg-[#0A66C2] hover:border-none"><Image width="24" height="24" src="/images/icons/linkedin.svg" className="text-white fill-current" /></div>
+            </a>
+            <a href="https://www.youtube.com/channel/UCl3-1zqMl6YkRx0OQ3KwC_g">
+              <div className="duration-75 flex items-center justify-center h-16 border border-gray-600 rounded cursor-pointer hover:bg-gray-800 hover:bg-[#FF0000] hover:border-none"><Image width="24" height="24" src="/images/icons/youtube.svg" /></div>
+            </a>
+            <a href="https://twitter.com/LeMathisBarre">
+              <div className="duration-75 flex items-center justify-center h-16 border border-gray-600 rounded cursor-pointer hover:bg-gray-800 hover:bg-[#1DA1F2] hover:border-none"><Image width="24" height="24" src="/images/icons/twitter.svg" /></div>
+            </a>
+            <a href="https://www.twitch.tv/mathisbarre_">
+              <div className="duration-75 flex items-center justify-center h-16 border border-gray-600 rounded cursor-pointer hover:bg-gray-800 hover:bg-[#9146FF] hover:border-none"><Image width="24" height="24" src="/images/icons/twitch.svg" /></div>
+            </a>
+            <a href="https://www.malt.fr/profile/mathisbarre">
+              <div className="duration-75 flex items-center justify-center h-16 border border-gray-600 rounded cursor-pointer hover:bg-gray-800 hover:bg-[#ff5c57] hover:border-none"><Image width="24" height="24" src="/images/icons/malt.svg" /></div>
+            </a>
+          </div>
+        </section>
+
 
         <h2 className="flex flex-col flex-wrap items-start mt-12 mb-2 text-xl font-bold md:items-center md:flex-row sm:text-3xl">
           <span>Engage in a discussion</span>
