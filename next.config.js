1efe4c2535bc991ae141c58c34a6312f1838d1c4
Mathis Barré <mathis.barre@live.fr>
2021-08-30 12:14:15 +0200

    feat: add splitbee analytics

--- next.config.js
+++ next.config.js
@@ -5,4 +5,16 @@ module.exports = withPWA({
     dest: "public",
     disable: process.env.NODE_ENV === "development",
   },
+  async rewrites() {
+    return [
+      {
+        source: "/bee.js",
+        destination: "https://cdn.splitbee.io/sb.js",
+      },
+      {
+        source: "/_hive/:slug",
+        destination: "https://hive.splitbee.io/:slug",
+      },
+    ];
+  },
 });
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
+import { useEffect } from 'react';
 import { AppProps } from 'next/app'
 import Head from "next/head"
 import '../styles/globals.css'
+import splitbee from '@splitbee/web';
 
 function App({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    splitbee.init({
+      scriptUrl: "/bee.js",
+      apiUrl: "/_hive",
+    })
+  }, [])
+
   return (
     <>
       <Head>
