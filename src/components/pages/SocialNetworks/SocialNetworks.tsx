6867efc9620744b7056f8c2060b6378fedfbe032
Mathis Barré <m.barre@beapp.fr>
2022-11-14 09:06:09 +0100

    content: fix twitter url

--- src/components/pages/SocialNetworks/SocialNetworks.tsx
+++ src/components/pages/SocialNetworks/SocialNetworks.tsx
@@ -10,10 +10,10 @@ export default function SocialNetworks() {
       <h2 className="title-section">Rejoignez-moi !</h2>
 
       <div className="grid grid-cols-3 gap-2 sm:grid-cols-6">
+        <SocialNetwork link="https://twitter.com/TheMathisBarre" iconUrl="/images/icons/twitter.svg" hoverClass="hover:bg-[#1DA1F2]" alt="Twitter" />
         <SocialNetwork link="https://github.com/MathisBarre" iconUrl="/images/icons/github.svg" hoverClass="hover:bg-[#181717]" alt="GitHub" />
         <SocialNetwork link="https://linkedin.com/in/mathis-barre" iconUrl="/images/icons/linkedin.svg" hoverClass="hover:bg-[#0A66C2]" alt="LinkedIn" />
         <SocialNetwork link="https://www.youtube.com/channel/UCl3-1zqMl6YkRx0OQ3KwC_g" iconUrl="/images/icons/youtube.svg" hoverClass="hover:bg-[#FF0000]" alt="YouTube" />
-        <SocialNetwork link="https://twitter.com/LeMathisBarre" iconUrl="/images/icons/twitter.svg" hoverClass="hover:bg-[#1DA1F2]" alt="Twitter" />
         <SocialNetwork link="https://www.twitch.tv/mathisbarre_" iconUrl="/images/icons/twitch.svg" hoverClass="hover:bg-[#9146FF]" alt="Twitch" />
         <SocialNetwork link="https://www.malt.fr/profile/mathisbarre" iconUrl="/images/icons/malt.svg" hoverClass="hover:bg-[#ff5c57]" alt="Malt" />
       </div>
